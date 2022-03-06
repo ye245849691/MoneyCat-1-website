f1fd7399a774d3caca1446a62989461fdc4561fd
Shawn <245849691@qq.com>
2022-03-07 01:07:17 +0800

    deploy

---
+++ precache-manifest.c36cf87f6298ca813bc1a4661a976ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defa8d61b5a75ce8c99f",
+    "url": "/MoneyCat-1-website/css/app.3c082e10.css"
+  },
+  {
+    "revision": "3a308ef028210bea622d1e1602a96438",
+    "url": "/MoneyCat-1-website/index.html"
+  },
+  {
+    "revision": "defa8d61b5a75ce8c99f",
+    "url": "/MoneyCat-1-website/js/app.8a1c3bd2.js"
+  },
+  {
+    "revision": "c84db525c11209891bb0",
+    "url": "/MoneyCat-1-website/js/chunk-vendors.bf2bd778.js"
+  },
+  {
+    "revision": "316dad224188d91d4b7fb337b528ca70",
+    "url": "/MoneyCat-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MoneyCat-1-website/robots.txt"
+  }
+]);
